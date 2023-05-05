f2b0e2c71b933111912bf554dfbf61d60d7bf7db
shrinidhi <21f1003929@student.onlinedegree.iitm.ac.in>
2023-05-05 13:51:13 +0530

    message

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="dpg-chaa3rbhp8u791gs1uu0-a",
+        host="dpg-chaa3rbhp8u791gs1uu0-a.oregon-postgres.render.com",
         database="shri",
         user="shrinidhi",
         password="uSWY3cVTgNkS3UZV3Ygo6RZKP91uADJB")
